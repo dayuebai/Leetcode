1157154fcfbdcaece76976520fa8fb507653ae5f
dayuebai <dayueb@uci.edu>
2019-02-11 11:09:35 -0800

    update explanation of jump gameII

--- Python/0045-jump-gameII.py
+++ Python/0045-jump-gameII.py
@@ -1,15 +1,25 @@
 # Time:  O(n)
 # Space: O(1)
-class Solution(object):
-    def jump(self, A):
-        jump_count = 0
-        reachable = 0
-        curr_reachable = 0
-        for i, length in enumerate(A):
-            if i > reachable:
-                return -1
-            if i > curr_reachable:
-                curr_reachable = reachable
-                jump_count += 1
-            reachable = max(reachable, i + length)
-        return jump_count
+class Solution:
+    # Greedy algorithm solution
+    def jump(self, nums: 'List[int]') -> 'int':
+        result = 0
+        curMaxJump = 0
+        maxJump = 0
+        size = len(nums)
+        
+        if size < 2:
+            return 0
+        for i in range(size-1):
+            maxJump = max(maxJump, i + nums[i])
+            if i == curMaxJump:
+                result += 1
+                curMaxJump = maxJump
+        return result
+
+    
+    # bfs solution
+    # different from greedy algorithm solution
+    # bfs loop through the range within current max
+    # if it find max > length - 1
+    # then it can terminate the loop earlier
