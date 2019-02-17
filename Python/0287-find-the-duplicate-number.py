79a889b827aeaf611522cfb2c934d91a9928597d
dayuebai <dayueb@uci.edu>
2019-02-16 20:47:00 -0800

    feb 16

---
+++ Python/0287-find-the-duplicate-number.py
@@ -0,0 +1,20 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def findDuplicate(self, nums):
+        # Treat each (key, value) pair of the array as the (pointer, next) node of the linked list,
+        # thus the duplicated number will be the begin of the cycle in the linked list.
+        # Besides, there is always a cycle in the linked list which
+        # starts from the first element of the array.
+        slow = nums[0]
+        fast = nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+        fast = 0
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow
